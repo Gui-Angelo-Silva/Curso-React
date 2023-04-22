aecbe75742f09ff11dd21382ac2156cebfc06438
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-04-22 10:50:17 -0300

    Aula 17 Curso React

--- src/components/basicos/Primeiro.js
+++
@@ -1,3 +0,0 @@
-export default function Primeiro(){
-    return 'Primeiro Componente'
-}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom' //o nome para o import é algo que queira
 import React from 'react' //o import do react tem q ser chamado assim 
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 //const el = document.getElementById('root')
 
@@ -16,6 +17,10 @@ import Primeiro from './components/basicos/Primeiro'
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo='Situação do Aluno' 
+            aluno='Guilherme Silva' 
+            nota ={9.3}/>
     </div>, 
     document.getElementById('root')
 )
