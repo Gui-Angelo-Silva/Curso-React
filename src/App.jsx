d8d9c9f4192415bfbdcceb065fe85eba9b4a25cd
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-04-22 16:05:21 -0300

    Aula 30 Curso React

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 import React from 'react'
 
 import Card from './components/layout/Card';
+
+import TabelaProdutos from './components/Repeticao/TabelaProdutos';
+import ListaAlunos from './components/Repeticao/ListaAlunos';
+import Familia from './components/basicos/Familia';
+import FamiliaMembro from './components/basicos/FamiliaMembro';
 import NumAleatorio from './components/basicos/NumAleatorio';
 import Fragmento from './components/basicos/Fragmento';
 import ComParametro from './components/basicos/ComParametro';
@@ -13,8 +18,28 @@ export default () => (
 
         <div className='Cards'>
             <Card
+                titulo='#07 - Desafio Repetição' color='#3A9AD9'>
+                <TabelaProdutos></TabelaProdutos>
+            </Card>
+
+            <Card
+                titulo='#06 - Repetição' color='#FF4C65'>
+                <ListaAlunos></ListaAlunos>
+            </Card>
+
+            <Card
+                titulo='#05 - Componentes com Filhos' color='#00C8F8'>
+                <Familia sobrenome='Angelo'>
+                    <FamiliaMembro nome='Gabriel' />
+                    <FamiliaMembro nome='Felipe' />
+                    <FamiliaMembro nome='Gustavo' />
+                </Familia>
+            </Card>
+
+            <Card
                 titulo='#04 - Desafio Aleatório' color='#FA6900'>
                 <NumAleatorio
+                    max={10}
                     min={1}
                 />
             </Card>
